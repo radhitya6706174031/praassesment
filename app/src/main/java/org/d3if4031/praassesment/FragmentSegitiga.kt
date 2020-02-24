3bd6f8795fa74f2fa55e6d22e87db49164bd00bb
Qibro <muhamad.ibrahim21@gmail.com>
2020-02-24 11:51:49 +0700

    add

---
+++ app/src/main/java/org/d3if4031/praassesment/FragmentSegitiga.kt
@@ -0,0 +1,30 @@
+package org.d3if4031.praassesment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class FragmentSegitiga : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_segitiga, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/org/d3if4031/praassesment/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.d3if4031.praassesment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
